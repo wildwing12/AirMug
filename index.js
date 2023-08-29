0480d97acc753d4031d35da820091b66b1910ec6
김종학 <ggipk@naver.com>
2023-08-29 09:01:20 +0900

    8월 29일

---
+++ index.js
@@ -0,0 +1,47 @@
+(()=>{
+    const sceneInfo = [
+        {
+            type:'sticky',
+            heightNum:5,//브라우저 높이의 5배로 scrollHight 세팅
+            scrollHeight:0,//하나의 씬의 높이
+            objs:{
+                container:document.querySelector("#scroll-section-0")
+            }
+        },
+        {
+            type:'normal',
+            heightNum:5,//브라우저 높이의 5배로 scrollHight 세팅
+            scrollHeight:0,//하나의 씬의 높이
+            objs:{
+                container:document.querySelector("#scroll-section-1")
+            }
+        },
+        {
+            type:'sticky',
+            heightNum:5,//브라우저 높이의 5배로 scrollHight 세팅
+            scrollHeight:0,//하나의 씬의 높이
+            objs:{
+                container:document.querySelector("#scroll-section-2")
+            }
+        },
+        {
+            type:'sticky',
+            heightNum:5,//브라우저 높이의 5배로 scrollHight 세팅
+            scrollHeight:0,//하나의 씬의 높이
+            objs:{
+                container:document.querySelector("#scroll-section-3")
+            }
+        }
+    ];
+
+    const setLayout = () =>{
+        for(let i = 0; i<sceneInfo.length; i++){
+            sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight; //window.innerHeight는 브라우저의 내부 높이다.
+            sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+        }
+        console.log(sceneInfo);
+    }
+
+    window.addEventListener('resize',setLayout);
+    setLayout();
+})();
