71f88ba42e6b977bf1a35f32ac7e3aa74c887265
kimjonghak <ggipk@naver.com>
2023-08-31 08:55:42 +0900

    8월 31일

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 (()=>{
     let yOffset = 0;
+    let prevScrollHeight = 0;//현재 스크롤 위치 (yOffSet)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+    let currentScene = 0 // 현재 활성화된 (눈 앞에 보고 있는)씬(scroll-section)
 
     const sceneInfo = [
         {
@@ -41,10 +43,21 @@
             sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight; //window.innerHeight는 브라우저의 내부 높이다.
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
         }
-        console.log(sceneInfo);
     }
     const scrollLope =() =>{
-
+        prevScrollHeight = 0;
+        //여기서 우리가 활성화 시킬 화면의 스크롤을 작성
+        for(let i = 0; i<currentScene; i++){
+            prevScrollHeight = prevScrollHeight + sceneInfo[i].scrollHeight;
+        }
+        if(yOffset>prevScrollHeight + sceneInfo[currentScene].scrollHeight){
+            currentScene ++;
+        }
+        if(yOffset < prevScrollHeight){
+            if(currentScene === 0) return;
+            currentScene--;
+        }
+        console.log(currentScene)
     }
 
     window.addEventListener('resize',setLayout);
